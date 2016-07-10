1b451b999a2b80eb61950ac6f450863c97b235c1
phillHgl <phill.n0oe@gmail.com>
2016-07-10 16:05:46 -0500

    add sctrips used to build wix4 projects

---
+++ CONTRIBUTING.md
@@ -0,0 +1,5 @@
+The wix4.MyResources is a collection of build scripts and test projects used when contributng to the WiXToolset wix4 repository.
+
+It may also be possible in the future to create example projects or extensions to share.
+
+At this time there are no plance to solicit contributions, but please feel free to contact me at phill dot hogland at rimage dot com, if you have the itch.
---
+++ Tools/Build.cmd
@@ -0,0 +1,34 @@
+@echo off
+setlocal
+:set the window size
+mode con: cols=210 lines=70
+
+::set PATH=%PATH%;%programfiles(x86)%\Microsoft Visual Studio 10.0\Common7\IDE
+::%ProgramFiles(x86)%\MSBuild\12.0\Bin
+::set _msbuild=%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
+::set _msbuild=%ProgramFiles(x86)%\MSBuild\12.0\Bin\msbuild.exe
+:: When VS2015 is installed, must used MSBuild v14
+set _msbuild=%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe
+if [_msbuild]==[] (
+  goto :errMBS
+)
+
+if not exist "%_msbuild%" (
+  goto :errMBS
+)
+
+call "%_msbuild%" ..\..\wix4\Wix.proj /t:Rebuild /p:PlatformToolset=v120_xp  /p:DebugSymbols=true /p:DebugType=full /p:Optimize=false /v:diag /l:FileLogger,Microsoft.Build.Engine;logfile="%~dp0\wix4.log"
+if errorlevel 1 goto :errBuild
+@echo 
+
+
+:errBuild
+@echo MSBuild returned an error.  Please review the log file.
+pause
+exit 2
+:errMBS
+@echo MSBuild was not located.  This script will exit.
+pause
+exit 1
+
+::endlocal implied
