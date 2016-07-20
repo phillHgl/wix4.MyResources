e2e25f47666a24d8c8271f90a0abe73375ceff5c
phillHgl <phill.n0oe@gmail.com>
2016-07-20 18:59:43 -0500

    added script to build the bundle

---
+++ Tools/Build_SetupBundle.cmd
@@ -0,0 +1,31 @@
+@echo off
+setlocal
+:set the window size
+mode con: cols=210 lines=70
+
+set _msbuild=%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe
+if [_msbuild]==[] (
+  goto :errMBS
+)
+
+if not exist "%_msbuild%" (
+  goto :errMBS
+)
+
+::/p:PlatformToolset=v120_xp 
+call "%_msbuild%" ..\..\wix4\src\Setup\Bundle\Bundle.wixproj /m:1 /p:DebugSymbols=true /p:DebugType=full /p:Optimize=false /v:normal /l:FileLogger,Microsoft.Build.Engine;logfile="%~dp0\Setup.log"
+if errorlevel 1 goto :errBuild
+
+::success
+exit /b 0
+
+:errBuild
+@echo MSBuild returned an error.  Please review the log file.
+pause
+exit 2
+:errMBS
+@echo MSBuild was not located.  This script will exit.
+pause
+exit 1
+
+::endlocal implied
